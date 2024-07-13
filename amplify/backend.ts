dd916689bd03cd9a815d843d70f9d9081570824d
Sapjarern <sapjarern081@gmail.com>
2024-07-13 06:51:12 +0000

    add env config

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -5,4 +5,12 @@ import { auth } from './auth/resource';
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({});
+const backend = defineBackend({});
+
+backend.addOutput({
+  auth: {
+    aws_region: process.env.REGION,
+    user_pool_id: process.env.USER_POOL_ID,
+    user_pool_client_id: process.env.USER_POOL_CLIENT_ID,
+  }
+})
