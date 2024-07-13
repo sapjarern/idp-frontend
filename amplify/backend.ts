bd2c5ffe9155184e219c17052a5d8fe30796e0a8
Sapjarern <sapjarern081@gmail.com>
2024-07-13 06:20:08 +0000

    remove auth backend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -5,7 +5,4 @@ import { auth } from './auth/resource';
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
-  auth,
-  // data,
-});
+defineBackend({});
